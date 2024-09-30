ef5dbb11548720709d24dfe2267a1f8ce7a8e752
selimtan <stanoncloud@gmail.com>
2024-09-30 14:18:48 +0300

    Prepare for version bump

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export * from './Adapters/MariaDB';
 
 export * from './Validators/Key'
 export * from './Validators/UID'
+export * from './Validators/Queries'
+export * from './Validators/Index'
 
 export * from './Exceptions'
